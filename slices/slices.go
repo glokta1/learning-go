29aedefa6046901b35c18787f6c209e05d347f2f
Rafey Ahmad <rafeyahmad@protonmail.com>
2024-01-02 01:59:49 +0530

    slices

---
+++ slices/slices.go
@@ -0,0 +1,20 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	pic := make([][]uint8, dy)
+	for i := 0; i < dy; i++ {
+		// initialize each row of pic with []uint8 slice of dx size
+		pic[i] = make([]uint8, dx)
+		for j := 0; j < dx; j++ {
+			pic[i][j] = uint8((i + j) / 2)
+		}
+	}
+
+	return pic
+}
+
+func main() {
+	pic.Show(Pic)
+}
